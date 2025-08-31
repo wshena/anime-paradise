94bfa831e39629ace5a9e0dad5042184a0465010
wshena <wshenaarrafi@gmail.com>
2025-08-31 11:28:54 +0700

    start:initializing project and installing required library

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.myanimelist.net',
-      },
-      {
-        protocol: 'https',
-        hostname: 'img.flawlessfiles.com',
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,116 +1,103 @@
-import Link from "next/link";
-import { Flex, Heading, Stack, VStack } from "@chakra-ui/react";
+import Image from "next/image";
 
-import JumbotronCarousel from "@/components/carousel/JumbotronCarousel";
-import ContentWrapper from "@/components/wrapper/ContentWrapper";
-import MainWrapper from "@/components/wrapper/MainWrapper";
-import CardCarousel from "@/components/carousel/CardCarousel";
-import SkeletonLoader from "@/components/loader/SkeletonLoader";
-import ReviewsCard from "@/components/cards/ReviewsCard";
-import BannerLink from "@/components/BannerLink";
-import ViewAllCollections from "@/components/ViewAllCollections";
-
-import { GetAnimeList, GetLatestReview, GetSeasonsNowList, GetTopAnime, GetUpcomingSeasonsAnime } from "@/utils/fetcher";
-import SingleAnimeBlock from "@/components/SingleAnimeBlock";
-import { animeBlackClover, animeDragonballDaima } from "@/utils/const";
-
-const LatestReview = ({latestReviews, sliceLatestReviews}:{latestReviews:any, sliceLatestReviews:any}) => {
+export default function Home() {
   return (
-    <Stack alignItems={'start'} gap={'20px'} width={'100%'}>
-      <Flex alignItems={'center'} justifyContent={'space-between'} width={'100%'}>
-        <Heading as={'h1'} fontWeight={'bold'} textTransform={'capitalize'} fontSize={{base:'1rem', md:'2rem', lg:'2rem'}} fontStyle={'italic'}>Latest Reviews</Heading>
-        <Link href={'/reviews?page=1'}>
-          <span className="text-[1rem] text-gray-500 hover:text-white">
-            View All
-          </span>
-        </Link>
-      </Flex>
-      {!latestReviews || latestReviews?.length <= 0 ? (
-        <Flex alignItems={'center'} justifyContent={'space-between'}>
-          {Array.from({length:4}).map((_, idx:number) => (
-            <SkeletonLoader key={idx} dimension={{
-              width: {base:'120px', md:'200px'},
-              height: {base:'200px', md:'280px'}
-            }} />
-          ))}
-        </Flex>
-      ) : (
-        <>
-          {sliceLatestReviews?.map((item:any) => (
-            <ReviewsCard data={item} key={item?.mal_id} />
-          ))}
-        </>
-      )}
-    </Stack>
-  )
-}
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
+          <li className="mb-2 tracking-[-.01em]">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+              src/app/page.tsx
+            </code>
+            .
+          </li>
+          <li className="tracking-[-.01em]">
+            Save and see your changes instantly.
+          </li>
+        </ol>
 
-const CarouselSection = ({data, label, url}:{data:any, label:string, url:string}) => {
-  return (
-    <>
-      {!data ? (
-        <Flex width={'100%'} alignItems={'center'} gap={'20px'}>
-          {Array.from({length:5}).map((_,idx:number) => (
-            <SkeletonLoader key={idx} dimension={{
-              width: {base:'120px', md:'200px'},
-              height: {base:'200px', md:'280px'}
-            }} />
-          ))}
-        </Flex>
-      ) : (
-        <CardCarousel initialData={data?.data} label={label} url={url} />
-      )}
-    </>
-  )
-};
-
-export default async function Home() {
-  const allListThisSeasons = await GetSeasonsNowList();
-  const latestReview = await GetLatestReview({ limit: 2 });
-  const sliceLatestReviews = latestReview?.data?.slice(0,2);
-  const mostPopularAnime = await GetTopAnime({
-    filter: 'bypopularity'
-  });
-  const mostPopularMovie = await GetTopAnime({
-    filter: 'bypopularity',
-    type: 'movie'
-  });
-  const latestCompleteAnime = await GetAnimeList({
-    status: 'complete',
-    order_by: 'end_date',
-    sort: 'desc'
-  });
-  const upcomingSeasonAnime = await GetUpcomingSeasonsAnime();
-  
-  return (
-    <MainWrapper>
-      {(!allListThisSeasons) ? (
-        <SkeletonLoader dimension={{ width: '100vw', height: { base: '400px', md: '500px' } }} />
-      ) : (
-        <JumbotronCarousel initialData={allListThisSeasons} />
-      )}
-      <ContentWrapper>
-        <VStack alignItems={'center'} gap={{base:'20px', md:'50px'}} width={'100%'} paddingY={{base:'30px', md:'50px'}}>
-          {/* this season */}
-          <CarouselSection data={allListThisSeasons} label="Latest Series From The Winter Season" url="/seasons/now" />
-          {/* most popular */}
-          <CarouselSection data={mostPopularAnime} label="Most Popular" url="/top/anime?filter=bypopularity" />
-          {/* latest complete */}
-          <CarouselSection data={latestCompleteAnime} label="Explore the Latest Fully Completed Anime" url="/anime?status=complete&order_by=end_date&sort=desc" />
-          
-          <BannerLink image="/image/DemonSlayer-InfinityMovie-Launch-Banner-2100x700-EN.avif" url={'https://www.demonslayer-movie.com/?utm_source=cap_cr&utm_medium=standard_banner_2&utm_campaign=demon-slayer-infinity-castle_en&referrer=cap_cr_standard_banner_2_demon-slayer-infinity-castle'} />
-          <SingleAnimeBlock data={animeDragonballDaima} image="/image/dargonBallDaima.avif" />
-          <BannerLink image="/image/Zenshu-S1-KV1-Banner-2100x700-EN.avif" url={'http://localhost:3000/anime/58502/zenshuu'} />
-          <SingleAnimeBlock data={animeBlackClover} image="/image/blackClover.avif" />
-          {/* most popular movies */}
-          <CarouselSection data={mostPopularMovie} label="Watch Popular Movies" url="/top/anime?filter=bypopularity&type=movie" />
-          {/* upcoming season anime */}
-          <CarouselSection data={upcomingSeasonAnime} label="Upcoming Season Anime" url="/seasons/upcoming" />
-          
-          <LatestReview latestReviews={latestReview} sliceLatestReviews={sliceLatestReviews} />
-          <ViewAllCollections />
-        </VStack>
-      </ContentWrapper>
-    </MainWrapper>
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
   );
 }
